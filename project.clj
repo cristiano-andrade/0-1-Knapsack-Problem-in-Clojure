b8fb678bf5059e7eb03ffa93ab3c32f4974f5527
Torey Hickman <torey@toreyhickman.com>
2013-09-09 00:57:33 -0500

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject knapsack "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject knapsack "0.1.0"
+  :description "Solution to 0-1 Knapsack Problem"
+  :url "https://github.com/toreyhickman/0-1-Knapsack-Problem-in-Clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
